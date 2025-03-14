38db0d1a8d189549a2aa53e447576931fbcf2306
vani <vani1306m@gmail.com>
2025-03-15 01:15:12 +0530

    Removed files and directory from old locations and moved them to core

--- cre8x/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('project/<int:project_id>/', views.chat_room, name='chat_room'),
-    path('load-messages/<int:room_id>/', views.load_messages, name='load_messages'),
-]
