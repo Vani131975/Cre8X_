02071532e87306e49917b8bbb0f58d11923cb745
vani <vani1306m@gmail.com>
2025-03-14 22:55:35 +0530

    save changes

--- cre8x/cre8x/wsgi.py
+++ cre8x/cre8x/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cre8x.cre8x.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cre8x.settings')
 
 application = get_wsgi_application()
