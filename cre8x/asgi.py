7505ec465046d8c978c997234ccaabf4fb8173f0
vani <vani1306m@gmail.com>
2025-03-15 10:00:01 +0530

    added requirements

--- cre8x/asgi.py
+++ cre8x/asgi.py
@@ -8,15 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cre8x.settings')
+
+# Initialize Django first
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+# Import channel components after Django is initialized
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cre8x.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
